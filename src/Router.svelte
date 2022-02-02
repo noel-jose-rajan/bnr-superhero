d14ec5bf0c2514625a221612084adb991cb7a3ff
= <=>
2022-02-03 03:13:10 +0530

    initial packages added

---
+++ src/Router.svelte
@@ -0,0 +1,16 @@
+<script>
+    import router from "page"
+	import Home from "./pages/Home.svelte"
+	import Login from "./pages/Login.svelte"
+
+    let page;
+
+	//Routing
+ 	router("/",      ()=>{page = Home})
+	router("/login", ()=>{ page = Login})
+	router.start()
+
+</script>
+
+
+<svelte:component this={page}  />
---
+++ src/pages/Home.svelte
@@ -0,0 +1,13 @@
+<script>
+   
+</script>
+
+<style lang="scss">
+    h1 {
+        font-weight: bold;
+        color: red;
+        font-size: 50px;
+    }
+</style>
+
+<h1>Home</h1>
